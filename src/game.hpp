aef75389fd6eed95f8fb3548eee96d3bc54dedcd
Surovi <ummekulsumsurovi@gmail.com>
2026-04-16 23:37:00 +0600

    chore(engine): add input handling and game state transitions

--- src/game.hpp
+++ src/game.hpp
@@ -13,6 +13,11 @@
 #include "screens.hpp"
 #pragma endregion
 
+#pragma region Globals
+#define ENTER_KEY 13
+#define ESC_KEY 27
+#pragma endregion
+
 enum class GameState
 {
    MENU,
@@ -40,6 +45,17 @@ public:
    {
       menuChickenTex.load("assets/textures/menu-chicken.png");
       grassTex.load("assets/textures/grass.png", true);
+   }
+
+   void init()
+   {
+      // Reset game state and score
+   }
+
+   void restartGame()
+   {
+      init();
+      state = GameState::PLAYING;
    }
 
    void render()
@@ -76,13 +92,72 @@ public:
       glutSwapBuffers();
    }
 
-   void handleKeyDown(unsigned char key, int x, int y) {}
+   void handleKeyDown(unsigned char key, int x, int y)
+   {
+      switch (state)
+      {
+      case GameState::MENU:
+         if (key == ENTER_KEY)
+            restartGame();
+         else if (key == 'h' || key == 'H')
+            state = GameState::HELP;
+         else if (key == ESC_KEY)
+            exit(0);
+         break;
+
+      case GameState::HELP:
+         if (key == ESC_KEY)
+            state = GameState::MENU;
+         break;
+
+      case GameState::PLAYING:
+         if (key == 'p' || key == 'P')
+            state = GameState::PAUSED;
+         else if (key == ESC_KEY)
+            state = GameState::MENU;
+
+      case GameState::GAME_OVER:
+         if (key == 'r' || key == 'R')
+            restartGame();
+         else if (key == ESC_KEY)
+            state = GameState::MENU;
+         break;
+
+      default:
+         break;
+      }
+      glutPostRedisplay();
+   }
 
    void handleKeyUp(unsigned char key, int x, int y) {}
 
-   void handleArrowKeyDown(int key, int x, int y) {}
+   void handleArrowKeyDown(int key, int x, int y)
+   {
+      if (state == GameState::PLAYING)
+      {
+         // if (key == MOUSE_LEFT)
+         //    move basket left
+         // else if (key == MOUSE_RIGHT)
+         //    move basket right
+      }
+   }
 
-   void handleArrowKeyUp(int key, int x, int y) {}
+   void handleArrowKeyUp(int key, int x, int y)
+   {
+      if (state == GameState::PLAYING)
+      {
+         // if (key == MOUSE_LEFT)
+         //    stop move basket left
+         // else if (key == MOUSE_RIGHT)
+         //    stop move basket right
+      }
+   }
 
-   void handleMouseMotion(int x, int y) {}
+   void handleMouseMotion(int x, int y)
+   {
+      if (state == GameState::PLAYING)
+      {
+         // move basket to x position
+      }
+   }
 };
